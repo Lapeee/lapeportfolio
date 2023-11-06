1fe7e9028feeadc24f965eb38ec282eaceb9c627
Lapeee <lauri.ilkka@gmail.com>
2023-11-06 16:59:43 +0200

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
